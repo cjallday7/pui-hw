d9f29a158fe5a3357a12ca547356c0a58b7a227c
Christian Johnson <33044116+cjallday7@users.noreply.github.com>
2024-09-20 12:12:05 -0700

    Create files

---
+++ solution-hw3/price-computation.js
@@ -0,0 +1,17 @@
+// const basePrice = 2.49;
+// const glazingPrice = document.getElementById(glaze-options);
+// const packSize = document.getElementsById(pack-size);
+
+//Glazing Option Variables
+const glazingOptions = querySelector("#glazing-options")
+const keepOriginal = querySelector("#keep-original");
+const sugarMilk = querySelector("#sugar-milk");
+const vanillaMilk = querySelector("#vanilla-milk");
+const doubleChocolate = querySelector("#double-chocolate");
+
+//Pack Size Variables
+const packSize = querySelector("#pack-size")
+const one = querySelector("#one");
+const three = querySelector("#three");
+const six = querySelector("#six");
+const twelve = querySelector("#twelve");
