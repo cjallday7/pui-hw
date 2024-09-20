6d3c34b32c7e96f27a30f34d1fb812c5f640a716
Christian Johnson <33044116+cjallday7@users.noreply.github.com>
2024-09-20 13:27:12 -0700

    Update files

--- solution-hw3/price-computation.js
+++ solution-hw3/price-computation.js
@@ -1,17 +1,54 @@
-// const basePrice = 2.49;
+const basePrice = 2.49;
+const updatedPrice = ((basePrice + all))
 // const glazingPrice = document.getElementById(glaze-options);
 // const packSize = document.getElementsById(pack-size);
 
+// When the page loads, find the select element.
+const glazeElement = document.querySelector("glaze-options");
+const packElement = document.querySelector("pack-size");
+
 //Glazing Option Variables
-const glazingOptions = querySelector("#glazing-options")
-const keepOriginal = querySelector("#keep-original");
-const sugarMilk = querySelector("#sugar-milk");
-const vanillaMilk = querySelector("#vanilla-milk");
-const doubleChocolate = querySelector("#double-chocolate");
+const allGlazeOptions = [
+    {
+        description: "Keep Original",
+        price: 0.00
+    },
+    {
+        description: "Sugar Milk",
+        price: 0.00
+    },
+    {
+        description: "Vanilla Milk",
+        price: 0.50
+    },
+    {
+        description: "Double Chocolate",
+        price: 1.50
+    }
+];
 
 //Pack Size Variables
-const packSize = querySelector("#pack-size")
-const one = querySelector("#one");
-const three = querySelector("#three");
-const six = querySelector("#six");
-const twelve = querySelector("#twelve");
+const allPackSizeOption = [
+    {
+        description: "one",
+        size: 1
+    },
+    {
+        description: "three",
+        size: 3
+    },
+    {
+        description: "six",
+        size: 6
+    },
+    {
+        description: "twelve",
+        size: 12
+    }
+];
+
+const glazeChoice  = document.querySelector("glaze-options")
+const packChoice = document.querySelector("pack-size")
+
+const glazeIndex = parse(this.value);
+const bunGlazePrice = basePrice + glazeIndex;
