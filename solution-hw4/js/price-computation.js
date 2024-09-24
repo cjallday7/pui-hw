3f25dd78d2e8e19ca77e044f12c6438c3c00eb37
Christian Johnson <33044116+cjallday7@users.noreply.github.com>
2024-09-24 15:01:49 -0400

    Creating files
    Creating files for Homework Assignment 4

---
+++ solution-hw4/js/price-computation.js
@@ -0,0 +1,79 @@
+//Base Price of the Original Cinnamon Roll
+const basePrice = 2.49;
+
+//Glazing Option Variables
+const allGlazeOptions = [
+    {
+        name: "Keep Original",
+        price: 0.00
+    },
+    {
+        name: "Sugar Milk",
+        price: 0.00
+    },
+    {
+        name: "Vanilla Milk",
+        price: 0.50
+    },
+    {
+        name: "Double Chocolate",
+        price: 1.50
+    }
+];
+
+//Pack Size Variables
+const allPackSizeOptions = [
+    {
+        description: "one",
+        size: 1
+    },
+    {
+        description: "three",
+        size: 3
+    },
+    {
+        description: "six",
+        size: 6
+    },
+    {
+        description: "twelve",
+        size: 12
+    }
+];
+    
+ // Populate the glazing dropdown (cite: w3schools.com)
+let glazingSelect = document.getElementById('glaze-options');
+for (let i = 0; i < allGlazeOptions.length; i++) {
+        let option = new Option(allGlazeOptions[i].name, allGlazeOptions[i].price);
+        glazingSelect.add(option);
+    }
+
+// Populate the pack size dropdown (cite: w3schools.com)
+let packSizeSelect = document.getElementById('pack-size');
+for (let i = 0; i < allPackSizeOptions.length; i++) {
+    let option = new Option(allPackSizeOptions[i].description, allPackSizeOptions[i].size);
+    packSizeSelect.add(option);
+    }
+
+// Function to update the price
+function updatePrice() {
+// Get the selected glazing price adaptation
+const glazingPrice = parseFloat(glazingSelect.value);
+        
+// Get the selected pack size price adaptation
+const packPrice = parseFloat(packSizeSelect.value);
+        
+// Calculate the total price
+const totalPrice = (basePrice + glazingPrice) * packPrice;
+    
+// Update the price in the HTML
+const priceDisplay = document.querySelector('.get-the-roll h3');
+priceDisplay.textContent = `${totalPrice.toFixed(2)}`;
+}
+
+// Add event listeners to trigger price update when user changes selections
+glazingSelect.addEventListener('change', updatePrice);
+packSizeSelect.addEventListener('change', updatePrice);
+
+// Update the price when the page first loads
+updatePrice();
