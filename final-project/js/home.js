6d85d98db7121c9d151103a1b17c956296393a23
Christian Johnson <33044116+cjallday7@users.noreply.github.com>
2024-12-08 16:32:20 -0500

    Update home.js
    adding show/hide functionality to tag buttons

--- final-project/js/home.js
+++ final-project/js/home.js
@@ -27,3 +27,12 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+function showHide() {
+  var x = document.getElementById("projects");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
